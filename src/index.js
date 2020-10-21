7251fda1582ba41baf6de780a93b2efdca2ad011
unknown <villagepllayer@gmail.comgit co>
2020-10-20 21:18:48 -0300

    Aula 9 Finalizada

--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './pages/home';
+import Login from './pages/Login';
+import Cadastrar from './pages/cadastrar';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+const routing = (
+  <Router>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/login' component={Login} />
+      <Route path='/cadastrar' component={Cadastrar} />
+  
+    </Switch>
+  </Router>
+)
 ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
+  routing,
   document.getElementById('root')
 );
 
